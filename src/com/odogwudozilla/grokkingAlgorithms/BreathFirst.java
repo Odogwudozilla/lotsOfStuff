72484c9e4554945c6a964ce38974df8532550301
Chidozie Nnachor <Chidozie.Nnachor@keylane.com>
2022-01-01 23:58:58 +0100

    grokkingAlgorithm - breatheFirst (still in progress)

--- src/com/odogwudozilla/grokkingAlgorithms/BreathFirst.java
+++ src/com/odogwudozilla/grokkingAlgorithms/BreathFirst.java
@@ -1,5 +1,6 @@
 package com.odogwudozilla.grokkingAlgorithms;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -7,7 +8,18 @@ import java.util.Map;
 import java.util.Queue;
 
 public class BreathFirst {
-	List<String> people = Arrays.asList("Anuj", "Bob", "Peggy", "Alice", "Claire", "Johnny", "Thom", "Odogwu");
+
+	Map<String, ArrayDeque<String>> people = new HashMap<>() {{
+		put("Anuj", new ArrayDeque<>(Arrays.asList("")));
+		put("Bob", new ArrayDeque<>(Arrays.asList("Anuj")));
+		put("Peggy", new ArrayDeque<>(Arrays.asList("")));
+		put("Alice", new ArrayDeque<>(Arrays.asList("Peggy")));
+		put("Claire", new ArrayDeque<>(Arrays.asList("Thom", "Johnny")));
+		put("Johnny", new ArrayDeque<>(Arrays.asList("")));
+		put("Thom", new ArrayDeque<>(Arrays.asList("")));
+		put("Odogwu", new ArrayDeque<>(Arrays.asList("Alice", "Claire", "Bob")));
+	}};
+
 	Map<String, List<String>> map1 = new HashMap<>();
 
 	public static void main(String[] args) {
@@ -16,7 +28,7 @@ public class BreathFirst {
 
 	}
 
-	private String determineMangoSeller(Queue<List<Map>> input) {
+	private String determineMangoSeller(Map<String, ArrayDeque<String>> input) {
 		return "testing";
 	}
 
